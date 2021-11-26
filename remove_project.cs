90ce35bdca8a19f64502d31f65d3988c2f59a887
mohamedomran22 <95096962+mohamedomran22@users.noreply.github.com>
2021-11-26 23:58:29 +0200

    Add files via upload

---
+++ remove_project.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> MoList = new LinkedList<string>();
+
+            LinkedListNode<string> MoNode = MoList.AddFirst("x1");
+            MoList.AddFirst("x2");
+            MoList.AddFirst("x3");
+            MoList.AddBefore(MoNode, "x4");
+            MoList.AddLast("losted");
+
+            Console.WriteLine("before removing linked list:  ");
+
+            foreach (var item in MoList)
+            {
+                Console.Write(item + ",");
+            }
+
+            MoList.RemoveFirst();
+            MoList.RemoveLast();
+            MoList.Remove("x4");
+            MoList.Remove("x1");
+
+            Console.WriteLine();
+            Console.WriteLine("After removing linked list: ");
+
+            foreach (var item in MoList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
